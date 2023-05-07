e32715d5a89e5d6e042e16c0b55dc057bc443761
AllenOtto <kephamoturi@gmail.com>
2023-05-07 16:37:25 +0300

    added ability to persist data in local storage

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('h2').innerHTML += "<span>"+ localStorage.bookTitle +"</span><br>"
 
 async function getFetch() {
     const isbn = document.querySelector('input').value
     const response = await fetch(`https://openlibrary.org/isbn/${isbn}.json`)
     const jsonData = await response.json()
     console.log(jsonData)
-    document.querySelector('h2').textContent = "Book Title: "+jsonData.title
+    localStorage.setItem("bookTitle", jsonData.title)
+    document.querySelector('h2').innerHTML = "Book Title: " + localStorage.bookTitle
     document.querySelector('h2').classList.add('focus')
 }
 
 // Examples of popular book isbn numbers
-// 9780980200447, 978-1-60309-515-0, 978-1-60309-469-6
+// 9780980200447, 978-1-60309-515-0, 978-1-60309-469-6, 978-1-60309-514-3, 
+// 978-1-60309-412-2
