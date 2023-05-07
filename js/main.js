bb51029aefc966e745c85f051e3b1c253ccd50c7
AllenOtto <kephamoturi@gmail.com>
2023-05-07 10:56:48 +0300

    added some stying and a bit of debugging

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 async function getFetch() {
     const isbn = document.querySelector('input').value
-    const response = await fetch(`https://openlibrary.org/${isbn}/9780140328721`)
+    const response = await fetch(`https://openlibrary.org/isbn/${isbn}.json`)
     const jsonData = await response.json()
-    console.log(jsonData.thumbnail_url)
-    document.querySelector('img').src = jsonData
+    console.log(jsonData)
+    document.querySelector('h2').textContent = "Book Title: "+jsonData.title
+    document.querySelector('h2').classList.add('focus')
 }
+
+// Examples of popular book isbn numbers
+// 9780980200447, 978-1-60309-515-0, 978-1-60309-469-6
